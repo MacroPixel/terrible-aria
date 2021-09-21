735f25058d2ab33fc62cff0caad2d63eed25ebde
Macro <58202313+MacroPixel@users.noreply.github.com>
2021-09-21 15:32:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from math import sin, cos
 import random
+import traceback
 
 # Returns a number such that min <= x <= max
 def clamp( n, mn, mx ):
@@ -12,22 +13,25 @@ def dist( a, b ):
 
   return ( ( a.x - b.x ) ** 2 + ( a.y - b.y ) ** 2 ) ** 0.5
 
-# Crushes a 2D index [x, y] down to a 1D index [c]
+# Flattens a 2D index [x, y] down to a 1D index [c]
 def xy2c( x, y, w ):
 
   return ( y * w ) + x
 
-# Converts a block's defining character into the character displayed in-game
+# Converts a block's character-ID into the character displayed in-game
 def char2tile( c ):
 
   if c == ' ': return g_tmap[ 'air' ]
   if c == 'g': return g_tmap[ 'grass' ]
   if c == 's': return g_tmap[ 'stone' ]
-  if c == 'w': return g_tmap[ 'log' ]
-  if c == 'l': return g_tmap[ 'leaves' ]
+  if c == 'l': return g_tmap[ 'log' ]
+  if c == 'L': return g_tmap[ 'leaves' ]
   if c == 'i': return g_tmap[ 'iron' ]
-  if c == 'v': return g_tmap[ 'silver' ]
-  if c == 'a': return g_tmap[ 'gold' ]
+  if c == 'S': return g_tmap[ 'silver' ]
+  if c == 'G': return g_tmap[ 'gold' ]
+  if c == 'w': return g_tmap[ 'wood' ]
+  if c == 'c': return g_tmap[ 'chest' ]
+  return '?'
 
 # Used for switching rooms
 class MoveException( BaseException ):
@@ -112,7 +116,10 @@ def print_progress( prog, div_this, div_total, s ):
   return prog
 
 # A function used for the top of the world
+# (A really interesting bug came up when I was integrating this function into worldgen,
+# so I may or may not make a supplemental video dedicated to explaining it)
 def noise_top( x, seed ):
+
 
   next_seed = random.randint( 1, 10 ** 12 )
   random.seed( seed )
@@ -128,6 +135,7 @@ g_data = {}
 g_cname = ''
 g_wname = ''
 g_pos = V2( 0, 0 )
+g_vel = V2( 0, 0 )
 g_view = V2( 0, 0 )
 g_world_size = V2( 0, 0 )
 g_hp = 0
@@ -145,16 +153,18 @@ g_tmap = {
   'leaves': '*',
   'iron': '&',
   'silver': '$',
-  'gold': '%'
+  'gold': '%',
+  'wood': '=',
+  'chest': '©'
 }
 
 # Initialize the data file if it doesn't exist
 def data_main_init():
 
-  file = open( 'data.txt', 'w' )
+  file = open( 'data.txt', 'w', encoding = 'utf-8' )
   file.write( 'characters: \n' )
   file.write( 'worlds: \n' )
-  file.write( 'blocks: player=Δ;air= ;grass=~;stone=#;log=];leaves=*;iron=&;silver=$;gold=%' )
+  file.write( 'blocks: player=Δ;air= ;grass=~;stone=#;log=];leaves=*;iron=&;silver=$;gold=%;wood==;chest=©' )
 
 # Loads character list, world list, and settings
 def data_main_load():
@@ -162,13 +172,13 @@ def data_main_load():
   global g_data, g_tmap
 
   # Initialize the data file if it doesn't exist
-  test = open( 'data.txt', 'a+' )
+  test = open( 'data.txt', 'a+', encoding = 'utf-8' )
   test.seek( 0 )
   if test.read() == '':
     data_main_init()
 
   # Read the character and world lists
-  file = open( 'data.txt', 'r' ).read().split( '\n' )
+  file = open( 'data.txt', 'r', encoding = 'utf-8' ).read().split( '\n' )
   g_data[ 'char_list' ] = file[0][ 12: ].split( ',' )
   if g_data[ 'char_list' ] == [ '' ]:
     g_data[ 'char_list' ] = []
@@ -179,12 +189,12 @@ def data_main_load():
   # Define the tile map
   t = file[2][ 8: ].split( ';' )
   for s in t:
-    g_tmap[ s.split( '=' )[0] ] = s.split( '=' )[1]
+    g_tmap[ s.split( '=' )[0] ] = ( s.split( '=' )[1] if len( s.split( '=' ) ) != 3 else '=' ) 
 
 # Write back to the data file if something changed
 def data_main_update():
 
-  file = open( 'data.txt', 'w' )
+  file = open( 'data.txt', 'w', encoding = 'utf-8' )
 
   # Character/world lists
   file.write( 'characters: ' )
@@ -230,10 +240,7 @@ def data_char_load( name ):
   # ..also play time
   g_play_time = int( file[2][11:] )
 
-def generate_world( file ):
-
-  world_size = V2( 400, 200 )
-  world_seed = random.randint( 1000, 9999 )
+def generate_world( size, seed ):
 
   # WORLDGEN PARAMETERS
   CAVES = True
@@ -244,33 +251,34 @@ def generate_world( file ):
   ORES = True
   ORES_FREQ = 128
 
-  global g_tile_data
+  global g_tile_data, g_pos
 
   # Clean slate
   g_tile_data = ''
   progress = 0
+  random.seed( seed )
   print( 'Generating World:' )
 
   # Fill world with air
-  for j in range( world_size.y ):
-    for i in range( world_size.x ):
+  for j in range( size.y ):
+    for i in range( size.x ):
       g_tile_data += ' '
-    progress = print_progress( progress, world_size.y, 5, 'Creating world array' ) # Update progress as it iterates
+    progress = print_progress( progress, size.y, 5, 'Creating world array' ) # Update progress as it iterates
   g_tile_data = list( g_tile_data )
 
   # Fill in area under the noise curve
-  for i in range( world_size.x ):
-    t = int( ( world_size.y / 2 ) + noise_top( i, world_seed ) )
+  for i in range( size.x ):
+    t = int( ( size.y / 2 ) + noise_top( i, seed ) )
 
     # 4 blocks of grass
     for j in range( t, t + 4 ):
-      g_tile_data[ xy2c( i, j, world_size.x ) ] = 'g'
+      g_tile_data[ xy2c( i, j, size.x ) ] = 'g'
 
     # And everything under it is stone
-    for j in range( t + 4, world_size.y ):
-      g_tile_data[ xy2c( i, j, world_size.x ) ] = 's'
+    for j in range( t + 4, size.y ):
+      g_tile_data[ xy2c( i, j, size.x ) ] = 's'
 
-    progress = print_progress( progress, world_size.x, 15, 'Generating initial terrain' ) # Update progress
+    progress = print_progress( progress, size.x, 15, 'Generating initial terrain' ) # Update progress
 
   # Generate iron, silver, and gold
   if ORES:
@@ -278,18 +286,18 @@ def generate_world( file ):
     for l1 in range( ORES_FREQ ): # Each iteration creates another vein
 
       # Create a vector at a random underground position
-      o = V2( random.randint( 0, world_size.x ), 0 )
-      o.y = random.randint( int( ( world_size.y / 2 ) + noise_top( o.x, world_seed ) ) + 10, world_size.y )
+      o = V2( random.randint( 0, size.x ), 0 )
+      o.y = random.randint( int( ( size.y / 2 ) + noise_top( o.x, seed ) ) + 10, size.y )
 
       d = 0 # This variable represents the direction (in radians)
 
       # Choose which kind of ore it is (depending on height)
-      if ( o.y / world_size.y < 0.65 ):
+      if ( o.y / size.y < 0.65 ):
         c = 'i'
-      elif ( o.y / world_size.y < 0.75 ):
-        c = random.choice( ( 'i', 'i', 'v' ) )
+      elif ( o.y / size.y < 0.75 ):
+        c = random.choice( ( 'i', 'i', 'S' ) )
       else:
-        c = random.choice( ( 'i', 'i', 'i', 'v', 'v', 'a' ) )
+        c = random.choice( ( 'i', 'i', 'i', 'S', 'S', 'G' ) )
 
       # This block moves the vector in a random direction on each iteration,
       # Which (somehow) usually leads to the blob-like shape I was going for
@@ -297,9 +305,9 @@ def generate_world( file ):
       for l2 in range( 48 ):
         d += random.randrange( -10, 10 ) 
         o.a( cos( d ), sin( d ) )
-        if o.y < ( world_size.y / 2 ) + noise_top( o.x, world_seed ) + 4:
+        if o.y < ( size.y / 2 ) + noise_top( o.x, seed ) + 4:
           continue
-        g_tile_data[ xy2c( clamp( int( o.x ), 1, world_size.x - 2 ), clamp( int( o.y ), 1, world_size.y - 2 ), world_size.x ) ] = c
+        g_tile_data[ xy2c( clamp( int( o.x ), 1, size.x - 2 ), clamp( int( o.y ), 1, size.y - 2 ), size.x ) ] = c
 
       progress = print_progress( progress, ORES_FREQ, 40, 'Generating ore veins' ) # Update progress
   else:
@@ -312,7 +320,7 @@ def generate_world( file ):
     for l1 in range( CAVES_FREQ ):
 
       # Create a vector in the bottom 30% of the world
-      o = V2( random.randint( 0, world_size.x ), random.randint( world_size.y * 0.7, world_size.y ) )
+      o = V2( random.randint( 0, size.x ), random.randint( size.y * 0.7, size.y ) )
 
       d = 0 # This variable represents the current direction (in radians)
       s = random.randrange( 2, 4 ) # This variable represents the current side (as a radiUS :D)
@@ -326,10 +334,10 @@ def generate_world( file ):
         o.a( cos( d ) * 2, sin( d ) * 2 )
         s += random.randrange( -10, 10 ) / 10
         s = clamp( s, 2, 4.5 )
-        for j in range( max( int( o.y ) - int( s ), 1 ), min( int( o.y ) + int( s ), world_size.y - 2 ) ):
-          for i in range( max( int( o.x ) - int( s ), 1 ), min( int( o.x ) + int( s ), world_size.x - 2 ) ):
+        for j in range( max( int( o.y ) - int( s ), 1 ), min( int( o.y ) + int( s ), size.y - 2 ) ):
+          for i in range( max( int( o.x ) - int( s ), 1 ), min( int( o.x ) + int( s ), size.x - 2 ) ):
             if dist( o, V2( i, j ) ) < s:
-              g_tile_data[ xy2c( i, j, world_size.x ) ] = ' '
+              g_tile_data[ xy2c( i, j, size.x ) ] = ' '
 
       progress = print_progress( progress, CAVES_FREQ, 20, 'Generating caves' ) # Update progress
 
@@ -344,13 +352,13 @@ def generate_world( file ):
 
       # This code works in the exact same way as the ore code
       # (I wrote this one first, though)
-      o = V2( random.randint( 0, world_size.x ), 0 )
-      o.y = random.randint( int( ( world_size.y / 2 ) + noise_top( o.x, world_seed ) ) + 10, world_size.y )
+      o = V2( random.randint( 0, size.x ), 0 )
+      o.y = random.randint( int( ( size.y / 2 ) + noise_top( o.x, seed ) ) + 10, size.y )
       d = 0
       for l2 in range( 64 ):
         d += random.randrange( -10, 10 ) 
         o.a( cos( d ), sin( d ) )
-        g_tile_data[ xy2c( clamp( int( o.x ), 1, world_size.x - 2 ), clamp( int( o.y ), 1, world_size.y - 2 ), world_size.x ) ] = ' '
+        g_tile_data[ xy2c( clamp( int( o.x ), 1, size.x - 2 ), clamp( int( o.y ), 1, size.y - 2 ), size.x ) ] = ' '
 
       progress = print_progress( progress, POCKETS_FREQ, 15, 'Generating air pockets' ) # Update progress
 
@@ -364,18 +372,18 @@ def generate_world( file ):
     i1 = random.randint( 5, 15 )
 
     # Continue until the position is within 15 blocks of the right bound
-    while i1 < world_size.x - 15:
+    while i1 < size.x - 15:
 
       # Create a vector at the current x position and the y position of the surface
-      o = V2( i1, int( ( world_size.y / 2 ) + noise_top( i1, world_seed ) - 1 ) )
+      o = V2( i1, int( ( size.y / 2 ) + noise_top( i1, seed ) - 1 ) )
 
-      if g_tile_data[ xy2c( o.x, o.y + 1, world_size.x ) ] == 'g': # Make sure there's grass below
+      if g_tile_data[ xy2c( o.x, o.y + 1, size.x ) ] == 'g': # Make sure there's grass below
 
         # Continue upward for a random height [10, 20]
         for i2 in range( random.randint( 10, 20 ) ):
 
           # Place down a log and move vector upward
-          g_tile_data[ xy2c( o.x, o.y, world_size.x ) ] = 'w'
+          g_tile_data[ xy2c( o.x, o.y, size.x ) ] = 'l'
           o.y -= 1
 
           # Decide whether the tree should sway a little
@@ -389,7 +397,7 @@ def generate_world( file ):
         for j in range( o.y - 4, o.y + 4 ):
           for i in range( o.x - 4, o.x + 4 ):
             if random.randint( 0, int( dist( V2( i, j ), o ) ) ) <= 1:
-              g_tile_data[ xy2c( i, j, world_size.x ) ] = 'l'
+              g_tile_data[ xy2c( i, j, size.x ) ] = 'L'
 
       # Choose how far to move before creating another tree
       i1 += int( random.randint( 8, 15 ) * random.choice( ( 0.6, 1, 1, 4 ) ) )
@@ -398,46 +406,50 @@ def generate_world( file ):
   else:
     progress = print_progress( progress, 1, 5, 'Generating trees' ) # Autofill progress if skipped
 
+  # Lastly, position the player
+  g_pos = V2( size.x // 2, int( ( size.y / 2 ) + noise_top( size.x // 2, seed ) ) - 1 )
+  g_tile_data[ xy2c( g_pos.x, g_pos.y + 1, size.x ) ] = 'g' # Make sure the player is standing on a block
+
   # Set progress to 100 (in case something glitched)
   print_progress( 50, 1, 1, 'This should never print' )
   print() # Newline
 
-  # Write buffer to world file
-  for j in range( world_size.y ):
-    file.write( ''.join( g_tile_data[ ( j * world_size.x ):( ( j + 1 ) * world_size.x ) ] ) )
-    file.write( '\n' )
-
 # Initialize a world file upon creation
-def data_world_init( name ):
+def data_world_init( name, seed ):
 
   file = open( 'w_' + name + '.txt', 'w' )
-  world_size = V2( 400, 200 )
+  size = V2( 400, 200 )
 
-  # Write non-tile details
-  file.write( f'world_size: { world_size.x },{ world_size.y }\n')
-  file.write( f'player_pos: { world_size.x // 2 },{ world_size.y // 2}\n' )
+  generate_world( size, seed )
 
-  # Write tiles
-  generate_world( file )
+  # Write non-tile data
+  file.write( f'size: { size.x },{ size.y }\n')
+  file.write( f'seed: { seed }\n')
+  file.write( f'player_pos: { g_pos.x },{ g_pos.y }\n' )
+  file.write( f'player_vel: 0,0\n' )
+
+  # Write tile data
+  for j in range( size.y ):
+    file.write( ''.join( g_tile_data[ ( j * size.x ):( ( j + 1 ) * size.x ) ] ) )
+    file.write( '\n' )
 
 # Load a world file with a given name
 def data_world_load( name ):
 
-  global g_pos, g_world_size, g_tile_data, g_show_help
+  global g_pos, g_vel, g_world_size, g_tile_data, g_show_help
 
   # Split file into statements
   file = open( 'w_' + name + '.txt', 'r' ).read().split( '\n' )
 
   # Read basic positional data
-  g_world_size.x = int( file[0][12:].split( ',' )[0] )
-  g_world_size.y = int( file[0][12:].split( ',' )[1] )
-  g_pos.x = int( file[1][12:].split( ',' )[0] )
-  g_pos.y = int( file[1][12:].split( ',' )[1] )
+  g_world_size = V2( int( file[0][6:].split( ',' )[0] ), int( file[0][6:].split( ',' )[1] ) )
+  g_pos = V2( int( file[2][12:].split( ',' )[0] ), int( file[2][12:].split( ',' )[1] ) )
+  g_vel = V2( int( file[3][12:].split( ',' )[0] ), int( file[3][12:].split( ',' )[1] ) )
 
   # Read tile data
   g_tile_data = "";
   for j in range( g_world_size.y ):
-    g_tile_data += file[2 + j]
+    g_tile_data += file[4 + j]
   g_tile_data = list( g_tile_data )
 
   g_show_help = True # Shows a help message on your first turn in the world
@@ -697,12 +709,35 @@ def room_world_create():
     elif name in g_data[ 'world_list' ]:
       print( '[#] This world already exists.' )
 
-    # Create file and reload world list
+    # Continue to other options
     else:
-      g_data[ 'world_list' ].append( name )
-      data_main_update()
-      data_world_init( name )
-      goto_room( room_world_select )
+      break
+
+  # Get seed
+  print( 'Enter a seed (blank = random):' )
+  while True:
+    seed = list( input( '> ' ).lower() )
+
+    # Numerify seed
+    # The seed needs to be a number, but instead of deleting all non-digit characters,
+    # they're instead converted to either 0 or their index within the alphabet :D
+    for i in range( len( seed ) ):
+      if seed[i] in list( 'abcdefghijklmnopqrstuvwxyz' ):
+        seed[i] = str( list( 'abcdefghijklmnopqrstuvwxyz' ).index( seed[i] ) + 1 )
+      elif seed[i] not in list( '0123456789' ):
+        seed[i] = '0'
+    if seed == []:
+      seed = random.randint( 1, 10 ** 32 )
+    else:
+      seed = int( ''.join( seed )[:32] )
+
+    # Continue to other options
+    break
+
+  g_data[ 'world_list' ].append( name )
+  data_main_update()
+  data_world_init( name, seed )
+  goto_room( room_world_select )
 
 #
 # WORLD SCENE ROOM
@@ -801,13 +836,17 @@ def room_scene():
             # Move in given direction and reload stage
             else:
               for i in range( t1 ):
-                if g_tile_data[ xy2c( g_pos.x + ( 1 if p.split( ' ' )[1][0] == 'r' else -1 ), g_pos.y, g_world_size.x ) ] == ' ':
-                  g_pos.x += ( 1 if t[0] == 'r' else -1 )
+                if g_tile_data[ xy2c( g_pos.x + ( 1 if p.split( ' ' )[1][0] == 'r' else -1 ), g_pos.y, g_world_size.x ) ] in [ ' ', 'l' ]:
+                  g_pos.x += ( 1 if p.split( ' ' )[1][0] == 'r' else -1 )
               goto_room( room_scene )
 
     # Invalid input
     else:
       print( '[#] Unknown command.' )
+
+def run_tick():
+
+  global g_pos, g_vel
 
 # Pause room
 def room_pause():
@@ -859,12 +898,13 @@ def run():
           break # Exit loop if user quit the game
 
   # Print crash message
-  except Exception as e:
-    print( '[CRASH] ' + str( e ) + '\n' )
+  except Exception:
+    print( '[CRASH] ' )
+    print( traceback.format_exc() )
 
   # Wait for input before ending program
   # (Windows consoles automatically close upon the end of a program,
-  # which isn't helpful if I want to be able to read a debug message.)
+  # which isn't helpful if I want to be able to read a crash message.)
   input( 'Press enter to exit.' )
 
 run()
